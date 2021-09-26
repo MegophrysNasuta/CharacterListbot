d443b9880f4d6d8c20e9c756564ca5de54e691e7
Megophrys Nasuta <nasuta@achaea.com>
2021-09-25 19:21:14 -0500

    Enhance update to collect death data

--- clist.py
+++ clist.py
@@ -48,6 +48,14 @@ def setup_db_if_blank(db_connection):
     cols = (('%s varchar(255) NOT NULL' % field) for field in API_FIELDS)
     sql += ', '.join(cols) + ');'
     db_connection.cursor().execute(sql)
+    sql = """
+    CREATE TABLE IF NOT EXISTS deaths (id serial PRIMARY_KEY,
+                                       killer varchar(255) NOT NULL,
+                                       corpse varchar(255) NOT NULL,
+                                       external_id varchar(255) NOT NULL,
+                                       timestamp datetime DEFAULT CURRENT_TIMESTAMP);
+    """
+    db_connection.cursor().execute(sql)
 
 
 def update_toon(db_connection, name):
@@ -58,6 +66,20 @@ def update_toon(db_connection, name):
                                           for field in API_FIELDS))
                     ), data)
     return data
+
+
+def get_or_create_deathsight(db_connection, killer, corpse, external_id):
+    cursor = db_connection.cursor()
+    cursor.execute('SELECT d.killer, d.corpse FROM deaths d WHERE d.external_id == ?;',
+                   (external_id,))
+    try:
+        killer, corpse = cursor.fetchall()[0]
+    except (IndexError, ValueError):
+        cursor.execute('INSERT INTO deaths (killer, corpse, external_id) VALUES (?, ?, ?)',
+                       (killer, corpse, external_id))
+        return True
+    else:
+        return False
 
 
 def get_or_create_toon(db_connection, name):
@@ -92,10 +114,18 @@ def list_toons(update=False, quick=False):
     return toon_list
 
 
-def show_game_feed():
+def show_death_history():
+    with sqlite3.connect('toons.db') as conn:
+        setup_db_if_blank(conn)
+        cursor = conn.cursor()
+        cursor.execute('SELECT killer, corpse from deaths')
+        return cursor.fetchall()
+
+
+def show_game_feed(types=('DEA', 'DUE')):
     url = '%s.json' % API_URL.replace('characters', 'gamefeed')
     data = requests.get(url).json()
-    return [row for row in data if row['type'] in ('DEA', 'DUE')]
+    return [row for row in data if row['type'] in types]
 
 
 def show_toon_archive():
@@ -136,11 +166,32 @@ if __name__ == '__main__':
             toons = list_toons(update=True)
             toon_list = list_toons(quick=True)
             print('%i toons updated!' % len(toon_list))
+            deaths_added = 0
+            for death in show_game_feed():
+                try:
+                    corpse, killer = death['description'].split(' was slain by ')
+                except ValueError:
+                    killer, corpse = death['description'].split(' defeated ')
+                    killer = killer.strip()
+                    corpse = corpse.split(None, 1)[0]
+                else:
+                    killer = killer.strip().rstrip('.')
+                    corpse = corpse.strip()
+
+                with sqlite3.connect('toons.db') as conn:
+                    setup_db_if_blank(conn)
+                    if get_or_create_deathsight(conn, external_id=str(death['id']),
+                                                killer=killer, corpse=corpse):
+                        deaths_added += 1
+            print('%i deaths added!' % deaths_added)
         elif arg.lower() == 'offline':
             toon_archive = show_toon_archive()
             for toon in toon_archive:
                 print(toon[1], end=', ')
             print('%i Achaeans known.' % len(toon_archive))
+        elif arg.lower() == 'deathhistory':
+            for death in show_death_history():
+                print('%s killed %s.' % death[:2])
         elif arg.lower() == 'gamefeed':
             for death in show_game_feed():
                 print(death['description'])

1b48214641e0ec34dc0c23e3afd55667b825556d
Megophrys Nasuta <nasuta@achaea.com>
2021-10-01 19:30:00 -0500

    I'll show you extend

--- dbot.py
+++ dbot.py
@@ -65,7 +65,7 @@ async def on_reaction_add(reaction, user):
         emoji = reaction.emoji
         args = create_pollopt(matches['poll_id'], emoji, emoji)
         msg = 'Poll option %i for poll %i is :%s: (%s)'
-        args.extend([reaction.name, reaction.name])
+        list(args).extend([reaction.name, reaction.name])
         await reaction.message.channel.send(msg % args)
 
     matches = POLL_OPEN_REGEX.match(reaction.message.content)
